f6a91eb56149d03cc8468e9f513e91daa2bb3ca6
SUSOT <a01076754042@gmail.com>
2025-02-17 22:41:14 +0900

    Fix : Player Counter Effect Bug

--- Assets/01.Script/LCM/Enemy/Bear/Bear.cs
+++ Assets/01.Script/LCM/Enemy/Bear/Bear.cs
@@ -1,8 +1,9 @@
 using System;
 using UnityEngine;
 
-public class Bear : Enemy
+public class Bear : Enemy, ICounterable
 {
+    private EntityAnimationTrigger _animationTrigger;
     protected override void Awake()
     {
         base.Awake();
@@ -27,6 +28,8 @@ public class Bear : Enemy
     {
         base.AfterInitialize();
         GetCompo<EntityHealth>().OnKnockback += HandleKnockBack;
+        _animationTrigger = GetCompo<EntityAnimationTrigger>();
+        _animationTrigger.OnCounterStatusChange += SetCounterStatus;
     }
 
     protected override void OnDestroy()
@@ -74,4 +77,21 @@ public class Bear : Enemy
         IsDead = true;
         TransitionState(EnemyStateType.Dead);
     }
+    #region Counter section
+    public bool CanCounter { get; private set; }
+    public void ApplyCounter(float damage, Vector2 direction, Vector2 knockBackForce, bool isPowerAttack, Entity dealer)
+    {
+        //damage에 스턴시간, 크리티컬 등등의 정보객체 넘어와야 하는데 지금은 damage만 주니까 하드코딩
+        float stunTime = 2f;
+
+        CanCounter = false;
+
+        GetCompo<EntityHealth>().ApplyDamage(damage, direction, knockBackForce, isPowerAttack, dealer);
+        Debug.Log("<color=green>Counter success</color>");
+    }
+
+    private void SetCounterStatus(bool canCounter)
+        => CanCounter = canCounter;
+
+    #endregion
 }
