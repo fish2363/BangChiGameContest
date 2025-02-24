0d2b4072ce6e6197cd3143130237469d35470510
SUSOT <a01076754042@gmail.com>
2025-02-24 22:58:27 +0900

    Feat : New Enemy Gladiator

---
+++ Assets/01.Script/LCM/Enemy/Gladiator/Gladiator.cs
@@ -0,0 +1,85 @@
+using System;
+using UnityEngine;
+
+public class Gladiator : Enemy, ICounterable
+{
+    private EntityAnimationTrigger _animationTrigger;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        foreach (EnemyStateType stateType in Enum.GetValues(typeof(EnemyStateType)))
+        {
+            try
+            {
+                string enumName = stateType.ToString();
+                Type t = Type.GetType($"Gladiator_{enumName}State");
+                EnemyState state = Activator.CreateInstance(t, new object[] { this }) as EnemyState;
+                StateEnum.Add(stateType, state);
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+        TransitionState(EnemyStateType.Idle);
+    }
+
+    protected override void AfterInitialize()
+    {
+        base.AfterInitialize();
+        GetCompo<EntityHealth>().OnKnockback += HandleKnockBack;
+        _animationTrigger = GetCompo<EntityAnimationTrigger>();
+        _animationTrigger.OnCounterStatusChange += SetCounterStatus;
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+        GetCompo<EntityHealth>().OnKnockback -= HandleKnockBack;
+        _animationTrigger.OnCounterStatusChange -= SetCounterStatus;
+    }
+
+    private void HandleKnockBack(Vector2 knockBackForce)
+    {
+        float knockBackTime = 0.5f;
+        KnockBack(knockBackForce, knockBackTime);
+    }
+
+    protected override void HandleHit()
+    {
+        
+    }
+
+    protected override void HandleDead() => Dead();
+
+    public override void Attack()
+    {
+        
+    }
+
+    public override void Dead()
+    {
+        if (IsDead) return;
+        gameObject.layer = DeadBodyLayer;
+        IsDead = true;
+        TransitionState(EnemyStateType.Dead);
+    }
+
+    #region Counter section
+    public bool CanCounter { get; private set; }
+    public void ApplyCounter(float damage, Vector2 direction, Vector2 knockBackForce, bool isPowerAttack, Entity dealer)
+    {
+        //damage에 스턴시간, 크리티컬 등등의 정보객체 넘어와야 하는데 지금은 damage만 주니까 하드코딩
+        float stunTime = 2f;
+
+        CanCounter = false;
+
+        GetCompo<EntityHealth>().ApplyDamage(damage, direction, knockBackForce, isPowerAttack, dealer);
+    }
+
+    private void SetCounterStatus(bool canCounter)
+        => CanCounter = canCounter;
+
+    #endregion
+}
