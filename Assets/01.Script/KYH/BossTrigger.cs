153ee7cd5876cda2e2c975f0cd50a5429bc8ffc6
fish2363 <a01022639410@gmail.com>
2025-02-27 18:48:57 +0900

    fix : Stage2 Song

--- Assets/01.Script/KYH/BossTrigger.cs
+++ Assets/01.Script/KYH/BossTrigger.cs
@@ -5,6 +5,7 @@ public class BossTrigger : MonoBehaviour
 {
     public GameObject Boss;
     public PlayableDirector director;
+    public string SongName;
 
     private void OnTriggerExit2D(Collider2D collision)
     {
@@ -19,7 +20,7 @@ public class BossTrigger : MonoBehaviour
     public void StartBoss()
     {
         Boss.SetActive(true);
-        AudioManager.Instance.PlaySound2D("SlimeKing",0,true,SoundType.BGM);
+        AudioManager.Instance.PlaySound2D("SongName",0,true,SoundType.BGM);
         FindAnyObjectByType<Player>().GetCompo<EntityMover>().CanManualMove = true;
     }
 }
